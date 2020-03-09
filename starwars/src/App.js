eef90cfea91f58f4123904903475f9ffccb5c554
Matthew Taylor <majikpig@gmail.com>
2020-03-08 21:59:54 -0400

    final commit

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -17,6 +17,8 @@ const App = () => {
   const [prevPage, setPrevPage] = useState('');
   const [films, setFilms] = useState([]);
 
+
+
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
